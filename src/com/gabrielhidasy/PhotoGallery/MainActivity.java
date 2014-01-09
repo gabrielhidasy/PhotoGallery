42539a113dc99e5c6f6e63c692125b10616212b7
Gabriel Hidasy <gabrielhidasy@gmail.com>
2014-01-09 20:34:44 -0200

    Funcionamento basico completo, mostra as fotos em fullscreen e troca quando toca na tela, vai servir para testar se realmente o consumo da tela muda com a cor, precisa fazer fotos no tamanho da tela do celular para não deixar borda preta, vou continuar tentando fazer trocar automatico, tambem preciso gravar eventos no log

--- src/com/gabrielhidasy/PhotoGallery/MainActivity.java
+++ src/com/gabrielhidasy/PhotoGallery/MainActivity.java
@@ -1,17 +1,47 @@
 package com.gabrielhidasy.PhotoGallery;
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
+import android.app.Activity;    
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.*;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity implements OnClickListener {
+
     /** Called when the activity is first created. */
+
+    int image_index = 0;
+    private static final int MAX_IMAGE_COUNT = 5;
+    private Integer[] mImageIds = {
+		R.raw.red,
+		R.raw.blue,
+		R.raw.green,
+		R.raw.black,
+		R.raw.white
+    };
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		View v = (View) findViewById(R.id.myimage);
+		v.setOnClickListener(this);
+        showImage();        
     }
-}
+
+    private void showImage() {             
+		ImageView imgView = (ImageView) findViewById(R.id.myimage);
+        imgView.setImageResource(mImageIds[image_index%5]);       
+
+    }
+    public void onClick(View v) {
+        image_index++;
+        if (image_index == MAX_IMAGE_COUNT) {               
+			image_index = 0;                
+			}
+            showImage();      
+        }
+	}
+
